568b6a24b4b2f77e4e634f7087773e36445db501
Jeffrey Charters <jeff.charters@gmail.com>
2022-01-12 17:53:26 -0500

    change counter location

--- src/routes/blurts/index.svelte
+++ src/routes/blurts/index.svelte
@@ -36,17 +36,19 @@
 <div class="max-w-md mx-auto">
 	<form class="flex justify-between items-baseline" on:submit|preventDefault={submitHandler}>
 		<label for="blurt" class="hidden">blurt</label>
-		<input
-			class="py-1 px-2 rounded-md border-2 border-solid border-teal-900 bg-teal-50 text-teal-800 font-semibold"
-			type="text"
-			bind:value={blurt}
-			id="blurt"
-			placeholder="blurt here"
-			autocomplete="off"
-			maxlength="14"
-			on:keyup={typeHandler}
-		/>
-		<div class="font-bold" style="color: hsl(190, 60%, 55%)" id="countdown-box">14</div>
+		<div class="flex items-baseline gap-2">
+			<input
+				class="py-1 px-2 rounded-md border-2 border-solid border-teal-900 bg-teal-50 text-teal-800 font-semibold w-40"
+				type="text"
+				bind:value={blurt}
+				id="blurt"
+				placeholder="blurt here"
+				autocomplete="off"
+				maxlength="14"
+				on:keyup={typeHandler}
+			/>
+			<div class="font-bold" style="color: hsl(190, 60%, 55%)" id="countdown-box">14</div>
+		</div>
 		<button
 			type="submit"
 			id="blurt-button"
