b7a51bcace137fd4d65d7472156fc7e26c99268e
Jeffrey Charters <jeff.charters@gmail.com>
2022-03-12 12:31:03 -0500

    yet another endpoint

--- src/lib/realtime.js
+++ src/lib/realtime.js
@@ -1,6 +1,6 @@
 import ioClient from 'socket.io-client';
 
-const ENDPOINT = 'https://letsblurt.duckdns.org:3005/';
+const ENDPOINT = 'https://letsblurt.duckdns.org/';
 
 const socket = ioClient(ENDPOINT);
 
