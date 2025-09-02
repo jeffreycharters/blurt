1ae5c41e9db2de69ae864426c18ef8460c4f3ee9
Jeffrey Charters <jeff.charters@gmail.com>
2025-09-02 19:06:21 -0400

    wip front start

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "../app.css"
+	import favicon from "$lib/assets/favicon.svg"
 
-	let { children } = $props();
+	let { children } = $props()
 </script>
 
 <svelte:head>
+	<title>Blurtonamo Bay</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<div class="min-h-screen min-w-full bg-teal-50 px-2 py-2">
+	{@render children?.()}
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let blurt = $state("")
+	let countdown_box: HTMLDivElement
+
+	function typeHandler() {
+		const remaining = 14 - blurt.length
+		const hue = (360 - 190) / remaining + 190
+		const saturation = (100 - 60) / remaining + 60
+
+		countdown_box.style.color = `hsl(${hue}, ${saturation}%, 50%)`
+		countdown_box.innerHTML = String(remaining)
+	}
+</script>
+
+<div class="mx-auto my-2 max-w-md px-2">
+	<form
+		class="flex items-baseline justify-between rounded bg-teal-100 p-2 shadow"
+		onsubmit={console.log}
+	>
+		<label for="blurt" class="sr-only">blurt</label>
+		<div class="flex items-baseline gap-2">
+			<input
+				class="w-40 rounded-md border border-solid border-teal-900 bg-teal-50 px-2 py-1 font-semibold text-teal-800"
+				type="text"
+				id="blurt"
+				bind:value={blurt}
+				placeholder="blurt here"
+				autocomplete="off"
+				maxlength="14"
+				onkeyup={typeHandler}
+			/>
+			<div class="font-bold" style="color: hsl(190, 60%, 55%)" bind:this={countdown_box}>14</div>
+		</div>
+
+		<button
+			type="submit"
+			id="blurt-button"
+			class="rounded-md border border-solid border-gray-900 bg-teal-600 px-4 py-1 font-semibold text-white"
+			>blurt it</button
+		>
+	</form>
+</div>
